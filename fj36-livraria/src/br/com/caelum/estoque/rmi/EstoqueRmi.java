f7070bfa476f9e804e3df1e31131ffd611c4f8b0
tominugen <kenzo.tominaga@gmail.com>
2017-08-12 16:59:12 -0300

    SOA mother flanders

---
+++ fj36-livraria/src/br/com/caelum/estoque/rmi/EstoqueRmi.java
@@ -0,0 +1,7 @@
+package br.com.caelum.estoque.rmi;
+
+public interface EstoqueRmi {
+
+	ItemEstoque getItemEstoque(String codigo);
+
+}
---
+++ fj36-livraria/src/br/com/caelum/estoque/rmi/ItemEstoque.java
@@ -0,0 +1,10 @@
+package br.com.caelum.estoque.rmi;
+
+public class ItemEstoque {
+
+	public Integer getQuantidade() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
